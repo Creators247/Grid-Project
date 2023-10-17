1b077cff7c0bcc1ba8b002e4fdd96045504e230a
Creators247 <greatamaizu@gmail.com>
2023-10-17 21:43:03 +0100

    3 files modified, 1 added

--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,7 +1,14 @@
-import { range } from './utils';
-
+import  range  from './utils';
+import './grid.css'
 function Grid({ numRows, numCols }) {
-  return <div className="grid">{/* TODO */}</div>;
+  return <div className="grid">
+    { range(numRows).map((each,index ) =>(
+       <div className="gridStyles" >{range(numCols).map(each => (
+         <div  className="gridStyles">great</div>
+       ))}</div>
+    )
+    )}
+  </div>;
 }
 
 export default Grid;
--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,12 @@
-export const range = (start, end, step = 1) => {
-  let output = [];
-  // solve this range utility
-
-  // handle scenario where end is not specified range(5) - [0, 1, 2, 3, 4]
-  // a conditional check for end
-
-  for (let i = start; i < end; i += step) {
-    // loop code
+export default function range (start ,end ,stop=1) {
+  let output = []
+  if(typeof end === 'undefined'){
+    end = start
+    start = 0
   }
+  for(let i = start; i < end ; i += stop ){
+    output.push(i);
+  }
+  return output
+}
 
-  return output;
-};
